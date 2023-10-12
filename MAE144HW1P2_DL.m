9e581e90091bccb91bd9aa56df4569845b31dcad
deh98 <dehao.lin0517@gmail.com>
2023-10-12 02:26:28 -0700

    Adjusted file names
    name corrected. Watched q3 videos, will get done tmr.

--- MAE144HW1P2_DL.m
+++
@@ -1,3 +0,0 @@
-%% Code for MAE 144 HW 1 Problem 2
-clear; close all; clc;
-
--- MAE144_HW1P2a.m
+++ MAE144_HW1P2a.m
@@ -1,6 +1,6 @@
 clear; close all; clc;
 
-%% MAE144 HW1 Problem1 
+%% MAE144 HW1 Problem2a 
 a = RR_poly([-1 1 -3 3 -6 6],1);
 b = RR_poly([-2 2 -5 5],1);
 f = RR_poly([-1,-1,-3,-3,-6,-6],1);
