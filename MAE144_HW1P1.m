b2bcfe669b0814efff88d0d20d19613af78cf59e
deh98 <dehao.lin0517@gmail.com>
2023-10-12 01:15:42 -0700

    1 & 2 done, working on 3
    Problem 3 is so hard.

---
+++ MAE144_HW1P1.m
@@ -0,0 +1,9 @@
+clear; close all; clc;
+
+%% MAE144 HW1 Problem1 
+a = RR_poly([-1 1 -3 3 -6 6],1);
+b = RR_poly([-2 2 -5 5],1);
+f = RR_poly([-1,-1,-3,-3,-6,-6],1);
+[x,y] = RR_diophantine(a,b,f)
+test = trim(a*x + b*y)
+residual1 = norm(f - test)
